23d54bdd5299ef53011cfcec3d79e588a2ddf57d
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-01 16:57:02 +0900

    show multiple crypto

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_course_bitcoin_ticker/coin_data.dart';
-import 'dart:io' show Platform;
+import 'package:universal_io/io.dart' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -60,26 +60,33 @@ class _PriceScreenState extends State<PriceScreen> {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 15.0,
-                  horizontal: 28.0,
-                ),
-                child: Text(
-                  '1 BTC = ? $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: cryptoList
+                  .map(
+                    (crypto) => Card(
+                      color: Colors.lightBlueAccent,
+                      elevation: 5.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 28.0,
+                        ),
+                        child: Text(
+                          '1 $crypto = ? $selectedCurrency',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
             ),
           ),
           Container(
